11d899cba24f37ea62c0a5bbaf13745e93ab838c
Rakshit Garg <raks.garg@gmail.com>
2014-08-14 22:37:00 -0700

    Added comments

--- Tipsy/ViewController.h
+++ Tipsy/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  Tipsy
 //
-//  Created by Gargium on 6/15/14.
+//  Created by Rakshit Garg on 6/15/14.
 //  Copyright (c) 2014 Gargium Interactive. All rights reserved.
 //
 
@@ -11,26 +11,30 @@
 
 @interface ViewController : UIViewController {
     
+//    create new slcomposeviewcontroller to handle the facebook
+//    and twitter requests in the .m file
+    
     SLComposeViewController *mySLComposerSheet;
 }
 
+//  declare properties for the UILabels
 @property (weak, nonatomic) IBOutlet UILabel *tipAmountField;
 @property (weak, nonatomic) IBOutlet UILabel *totalBillAmount;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 
+// declare properties for the UITextFields (which is where user will be entering data)
 @property (weak, nonatomic) IBOutlet UITextField *enteredBill;
 @property (weak, nonatomic) IBOutlet UITextField *tipField;
 @property (weak, nonatomic) IBOutlet UITextField *splitField;
 
+//  declare property for UIButton, which will be used to calculate tip
 @property (weak, nonatomic) IBOutlet UIButton *calculateBill;
 
 
-
+//  declare actions that will be implemented in the .m file
 - (IBAction)calculateBill:(id)sender;
 - (IBAction)postToTwitter:(id)sender;
 - (IBAction)postToFacebook:(id)sender;
-
-
 
 
 @end
--- Tipsy/ViewController.m
+++ Tipsy/ViewController.m
@@ -2,21 +2,23 @@
 //  ViewController.m
 //  Tipsy
 //
-//  Created by Gargium on 6/15/14.
+//  Created by Rakshit Garg on 6/15/14.
 //  Copyright (c) 2014 Gargium Interactive. All rights reserved.
 //
 
 // FOR FUTURE VERSIONS: Add a "save location" functionality and a settings bar to set certain defaults.
 
 #import "ViewController.h"
+
+//  Necessary import to use the Facebook and Twitter features
 #import <Social/Social.h>
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
+//  synthesis of properties that were declared in ViewController.h file
 @synthesize enteredBill, tipField, splitField, totalLabel;
 @synthesize tipAmountField, totalBillAmount;
 
@@ -25,10 +27,11 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+//  Calls the removeNumberPad method when the user taps out of the keyboard
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(removeNumberPad)];
     [self.view addGestureRecognizer:tap];
    
-    //add background image to view
+//  add background image to view
     UIImage *backgroundImageGold = [UIImage imageNamed:@"TipsyBG.png"];
     UIImageView *backgroundImageView = [[UIImageView alloc] initWithFrame:self.view.frame];
     backgroundImageView.image = backgroundImageGold;
@@ -41,21 +44,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+//  Action that is triggered when "Calculate" button is pressed
 -(IBAction)calculateBill:(id)sender {
-    
+
+//  Call to removeNumberPad, allowing keyboard to exit when the user presses the calculate button
+//  This allows the user to actually see the tip, rather than just the keyboard
     [self removeNumberPad];
+
+//  Initialization of variables
+    double bill = 0.0;  //  The after-tax total
+    double tip = 0.0;   //  Amount that the user wants to tip (in %)
+    int split = 0;  //  Number of people user will split the bill with (including user)
+    double total = 0.0; // Total cost
     
-    double bill = 0.0;
-    double tip = 0.0;
-    int split = 0;
-    double total = 0.0;
-    
+//  Retrieves values in double format from the textFields
     bill = [enteredBill.text doubleValue];
     tip = [tipField.text doubleValue];
     split = [splitField.text intValue];
     
+//  Error handling logic, just in case the user accidentally types in 0 for the "Split" field
     if (split <= 0) {
         
+//  Creates a popup alert, notifying user to type in a positive integer for Split
         UIAlertView *errorMessage = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Split must be a positive integer!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [errorMessage show];
         //[errorMessage release];
@@ -71,23 +81,28 @@
     
 }
 
-- (IBAction)postToTwitter:(id)sender {
-//        if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
-//        
-//        SLComposeViewController *tweeter = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-//        [tweeter setInitialText:@"Had a great time eating at (insert location here)!"];
-//        [self presentViewController:tweeter animated:YES completion:nil];
+//  Posts to twitter
 
+- (IBAction)postToTwitter:(id)sender {
+    
+//  Checking if Twitter account is available on device
     
     if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
-        mySLComposerSheet = [[SLComposeViewController alloc] init];
-        mySLComposerSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-        [mySLComposerSheet setInitialText:[NSString stringWithFormat:@"Had a great time eating at (insert venue here)!", mySLComposerSheet.serviceType]];
+        
+        mySLComposerSheet = [[SLComposeViewController alloc] init]; // Initiate Social Controller
+        mySLComposerSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter]; //Specify that we want Twitter,
+//  not an alternate Social Network
+        
+        [mySLComposerSheet setInitialText:[NSString stringWithFormat:@"Had a great time eating at (insert venue here)!", mySLComposerSheet.serviceType]]; //Default text that will show up in the box
+        
         [self presentViewController:mySLComposerSheet animated:YES completion:nil];
     }
     
     [mySLComposerSheet setCompletionHandler:^(SLComposeViewControllerResult result) {
         NSString *output;
+       
+//  BASIC ERROR MANAGEMENT:
+//  Returns a popup alert notifying the user whether the post was successful or if the action was cancelled
         switch (result) {
             case SLComposeViewControllerResultCancelled:
                 output = @"Action Cancelled";
@@ -105,38 +120,8 @@
 }
 
 
-
-//if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) //check if Facebook Account is linked
-//{
-//    mySLComposerSheet = [[SLComposeViewController alloc] init]; //initiate the Social Controller
-//    mySLComposerSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook]; //Tell him with what social plattform to use it, e.g. facebook or twitter
-//    [mySLComposerSheet setInitialText:[NSString stringWithFormat:@"Test",mySLComposerSheet.serviceType]]; //the message you want to post
-//    [mySLComposerSheet addImage:yourimage]; //an image you could post
-//    //for more instance methodes, go here:https://developer.apple.com/library/ios/#documentation/NetworkingInternet/Reference/SLComposeViewController_Class/Reference/Reference.html#//apple_ref/doc/uid/TP40012205
-//    [self presentViewController:mySLComposerSheet animated:YES completion:nil];
-//}
-//[mySLComposerSheet setCompletionHandler:^(SLComposeViewControllerResult result) {
-//    NSString *output;
-//    switch (result) {
-//        case SLComposeViewControllerResultCancelled:
-//            output = @"Action Cancelled";
-//            break;
-//        case SLComposeViewControllerResultDone:
-//            output = @"Post Successfull";
-//            break;
-//        default:
-//            break;
-//    } //check if everything worked properly. Give out a message on the state.
-//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Facebook" message:output delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
-//    [alert show];
-//}];
-
-
 - (IBAction)postToFacebook:(id)sender {
-//    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
-//        SLComposeViewController *fb = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
-//        [fb setInitialText:@"Had a great time eating at (instert location here)!"];
-//        [self presentViewController:fb animated:YES completion:nil];
+
     
     if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
         mySLComposerSheet = [[SLComposeViewController alloc] init];
@@ -163,11 +148,7 @@
     
 }
 
-
-
-
-
-
+//  Method that hides the keyboard
 
 -(void) removeNumberPad {
     
